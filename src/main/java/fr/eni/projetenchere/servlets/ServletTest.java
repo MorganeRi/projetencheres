a20d8a8df698d15b5f74eaf77c0b2923f6dfb578
hherve2023 <hherve2023@129SE2-BD4MMS3.ad.campus-eni.fr>
2023-03-27 11:54:32 +0200

    J'entre en conflit !!

--- src/main/java/fr/eni/projetenchere/servlets/ServletTest.java
+++ src/main/java/fr/eni/projetenchere/servlets/ServletTest.java
@@ -25,7 +25,7 @@ public class ServletTest extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+System.out.println("prout");
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
