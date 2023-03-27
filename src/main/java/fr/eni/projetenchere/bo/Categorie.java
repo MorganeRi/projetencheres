84d6438c99d176d83bbb567a55955721722a9798
hherve2023 <hherve2023@129SE2-BD4MMS3.ad.campus-eni.fr>
2023-03-27 15:01:20 +0200

    BO Categorie, enchere et retrait

--- src/main/java/fr/eni/projetenchere/bo/Categorie.java
+++ src/main/java/fr/eni/projetenchere/bo/Categorie.java
@@ -1,5 +1,80 @@
 package fr.eni.projetenchere.bo;
 
+import java.util.List;
+
 public class Categorie {
 
+	private Integer noCategorie;
+	private String libelle;
+	private List<ArticleVendu> ListeArticles;
+	
+	//Plusieurs constructeurs differents
+	
+	//complet
+	public Categorie(Integer noCategorie, String libelle, List<ArticleVendu> listeArticles) {
+		super();
+		this.noCategorie = noCategorie;
+		this.libelle = libelle;
+		ListeArticles = listeArticles;
+	}
+	//sans id
+	public Categorie(String libelle, List<ArticleVendu> listeArticles) {
+		super();
+		this.libelle = libelle;
+		ListeArticles = listeArticles;
+	}
+	//sans list
+	public Categorie(Integer noCategorie, String libelle) {
+		super();
+		this.noCategorie = noCategorie;
+		this.libelle = libelle;
+	}
+	//san id sans list
+	public Categorie(String libelle) {
+		super();
+		this.libelle = libelle;
+	}
+	//vide
+	public Categorie() {
+		super();
+	}
+
+	//Getters and Setters
+	
+	public Integer getNoCategorie() {
+		return noCategorie;
+	}
+
+	public void setNoCategorie(Integer noCategorie) {
+		this.noCategorie = noCategorie;
+	}
+
+	public String getLibelle() {
+		return libelle;
+	}
+
+	public void setLibelle(String libelle) {
+		this.libelle = libelle;
+	}
+
+	public List<ArticleVendu> getListeArticles() {
+		return ListeArticles;
+	}
+
+	public void setListeArticles(List<ArticleVendu> listeArticles) {
+		ListeArticles = listeArticles;
+	}
+
+	//To string 
+	
+	@Override
+	public String toString() {
+		return "Categorie [noCategorie=" + noCategorie + ", libelle=" + libelle + ", ListeArticles=" + ListeArticles
+				+ "]";
+	}
+	
+
+	
+	
+	
 }
