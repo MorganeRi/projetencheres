b7512050c39f98046bbfce4ab885638df769cdc5
hherve2023 <hherve2023@129SE2-BD4MMS3.ad.campus-eni.fr>
2023-03-28 09:21:46 +0200

    MAJ gestion message erreurs

--- src/main/java/fr/eni/projetenchere/dal/CodesResultatDAL.java
+++ src/main/java/fr/eni/projetenchere/dal/CodesResultatDAL.java
@@ -4,17 +4,48 @@ package fr.eni.projetenchere.dal;
  * Les codes disponibles sont entre 10000 et 19999
  */
 public abstract class CodesResultatDAL {
+	/**
+	 * Echec général quand tentative d'ajouter un article null
+	 */
+	public static final int INSERT_ARTICLE_NULL=10000;
 	
 	/**
-	 * Echec général quand tentative d'ajouter un objet null
+	 * Echec général quand tentative de supprimer un article null
 	 */
-	public static final int INSERT_OBJET_NULL=10000;
+	public static final int DELETE_ARTICLE_NULL=10010;
+	/**
+	 * Echec général quand tentative de update un article null
+	 */
+	public static final int UPDATE_ARTICLE_NULL=10020;
+	/**
+	 * Echec général quand tentative de update un article null
+	 */
+	public static final int UPDATE_PX_VENTE_ARTICLE_NULL=10030;
 	
 	/**
-	 * Echec général quand erreur non gérée à l'insertion 
+	 * Echec général quand erreur non gérée à l'insertion d'un article
 	 */
-	public static final int INSERT_OBJET_ECHEC=10001;
-	
+	public static final int INSERT_ARTICLE_ECHEC=10001;
+	/**
+	 * Echec général quand erreur non gérée à l'insertion d'un article
+	 */
+	public static final int DELETE_ARTICLE_ECHEC=10011;
+	/**
+	 * Echec de l'insertion d'un avis à cause de la note
+	 */
+	public static final int UPDATE_ARTICLE_ECHEC=10021;
+	/**
+	 * Echec de l'insertion d'un avis à cause de la note
+	 */
+	public static final int UPDATE_PX_VENTE_ARTICLE_ECHEC=10031;
+	/**
+	 * Echec de l'insertion d'un avis à cause de la note
+	 */
+	public static final int SELECT_ALL_ARTICLE_ECHEC=10003;
+	/**
+	 * Echec de l'insertion d'un avis à cause de la note
+	 */
+	public static final int SELECT_ALL_LISTE_ECHEC=10005;
 	/**
 	 * Echec de l'authentification
 	 */
