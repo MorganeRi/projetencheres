a00309ee4124d8e02c9e97bb66c57a953f5df627
mrichou2023 <mrichou2023@129SE2-6D4MMS3.ad.campus-eni.fr>
2023-03-28 16:15:41 +0200

    1

--- src/main/java/fr/eni/projetenchere/bll/UtilisateurManager.java
+++ src/main/java/fr/eni/projetenchere/bll/UtilisateurManager.java
@@ -1,5 +1,32 @@
 package fr.eni.projetenchere.bll;
 
+import fr.eni.projetenchere.BusinessException;
+import fr.eni.projetenchere.bo.Utilisateur;
+
 public interface UtilisateurManager {
+	
+	
+	/*Contraintes createUtilisateur : 
+	 * - email non null et pas de doublon -> Créer méthode pour réutiliser
+	 * - pseudo unique, accepte uniquement les caractères alphanumérique
+	 * - Crédit de 100 points par défaut à la création */
+	public void createUtilisateur (Utilisateur utilisateur) throws BusinessException;
+	
+	
+	// voir méthode du prof
+	public void authentifierUtilisateur (String email, String motDePasse)throws BusinessException;
+	
+	//Même contraintes que pour la création
+	public void majUtilisateur (Utilisateur utilisateur) throws BusinessException;
+	
+	public void validerEmail (Utilisateur utilisateur, BusinessException businessException);
+	
+	public void validerPseudo (Utilisateur utilisateur, BusinessException businessException);
+	
+	public void supprimerUtilisateur (Utilisateur utilisateur) throws BusinessException;
+	
+	public Utilisateur selectParNoUtilisateur (Integer id) throws BusinessException;
+	
+	public void majMontantCredit (Utilisateur utilisateur)throws BusinessException;
 
 }
--- src/main/java/fr/eni/projetenchere/servlets/tests/ServletTestDal.java
+++ src/main/java/fr/eni/projetenchere/servlets/tests/ServletTestDal.java
@@ -49,7 +49,7 @@ public class ServletTestDal extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) {
 
-		Utilisateur utilisateur = new Utilisateur("Momo", "Richou", "Morgane", "kjgsfgdfl.com", "0666666666",
+		Utilisateur utilisateur = new Utilisateur("Momo", "Richou", "Morgane", "kjgsfgdfm.com", "0666666666",
 				"rue de Lionel Richou", "66666", "LA", "coucouCmoi", 100, false);
 
 		UtilisateurDAO utilDAO = new UtilisateurDAOJdbcImpl();
@@ -112,7 +112,7 @@ public class ServletTestDal extends HttpServlet {
 			Utilisateur ut = new Utilisateur();
 			ut = utilDAO.selectByIdUtilisateur(5);
 			List<ArticleVendu> nart = new ArrayList<ArticleVendu>();
-			nart= dao.selectByNomArticle("out");
+			nart= dao.selectByNomArticle("ane");
 			List<Enchere> ench = new ArrayList<Enchere>();
 			ench = enchDAO.selectEnchereById(a1);
 			System.out.println(ut.toString());
