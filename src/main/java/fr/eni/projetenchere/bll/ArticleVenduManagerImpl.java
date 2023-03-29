6e042016c9c5f46253de6f7a7c3ca2f1458b6797
mrichou2023 <mrichou2023@129SE2-6D4MMS3.ad.campus-eni.fr>
2023-03-29 10:12:43 +0200

    MAJ bll morgane

--- src/main/java/fr/eni/projetenchere/bll/ArticleVenduManagerImpl.java
+++ src/main/java/fr/eni/projetenchere/bll/ArticleVenduManagerImpl.java
@@ -1,5 +1,63 @@
 package fr.eni.projetenchere.bll;
 
-public class ArticleVenduManagerImpl {
+import java.util.List;
+
+import fr.eni.projetenchere.BusinessException;
+import fr.eni.projetenchere.bo.ArticleVendu;
+import fr.eni.projetenchere.bo.Categorie;
+import fr.eni.projetenchere.dal.ArticleVenduDAO;
+import fr.eni.projetenchere.dal.DAOFactory;
+
+public class ArticleVenduManagerImpl implements ArticleVenduManager {
+
+	private ArticleVenduDAO dao = DAOFactory.getArticleVenduDAO();
+
+	@Override
+	public void ajouterArticleVendu(ArticleVendu article) throws BusinessException {
+		
+//		BusinessException businessException = new BusinessException();
+//		if (!businessException.hasErreurs()) {
+			this.dao.insertArticleVendu(article);
+//		} else {
+//			throw businessException;
+//		}
+
+	}
+
+	@Override
+	public void majArticleVendu(ArticleVendu article) throws BusinessException {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void supprimerArticleVendu(ArticleVendu article) throws BusinessException {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public List<ArticleVendu> selectParCategorieArticle(Categorie categorie) throws BusinessException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<ArticleVendu> selectParNomArticle(String nom) throws BusinessException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void majPxVenteArticleVendu(ArticleVendu articleVendu) throws BusinessException {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public ArticleVendu selectParIdArticle(Integer id) throws BusinessException {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
