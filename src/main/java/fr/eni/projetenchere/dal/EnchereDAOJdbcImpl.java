01b5686c408edb71a6353fbefaf18968177fdd86
mrichou2023 <mrichou2023@129SE2-6D4MMS3.ad.campus-eni.fr>
2023-04-03 11:16:00 +0200

    maj

--- src/main/java/fr/eni/projetenchere/dal/EnchereDAOJdbcImpl.java
+++ src/main/java/fr/eni/projetenchere/dal/EnchereDAOJdbcImpl.java
@@ -11,33 +11,33 @@ import fr.eni.projetenchere.bo.ArticleVendu;
 import fr.eni.projetenchere.bo.Enchere;
 import fr.eni.projetenchere.bo.Utilisateur;
 
-public class EnchereDAOJdbcImpl implements EnchereDAO {
+public class EnchereDAOJdbcImpl implements EnchereDAO{
 
-	private static final String SELECT_ENCHERE_BY_ID_ARTICLE = "SELECT  e.no_enchere,e.date_enchere, e.montant_enchere,e.no_article,e.no_utilisateur FROM enchere AS e WHERE no_article=?";
-	private static final String INSERT_ENCHERE = " INSERT INTO enchere (date_enchere, montant_enchere,no_utilisateur,no_article) VALUES (?,?,?,?)";
-	private static final String SELECT_MAX_ENCHERE = "select no_enchere, date_enchere, MAX(montant_enchere),no_utilisateur,no_article, enchere_gagnante from enchere WHERE no_article=?";
-	private static final String SELECT_MAX_UTILISATEUR = "select no_enchere, date_enchere, MAX(montant_enchere),no_utilisateur,no_article, enchere_gagnante from enchere WHERE no_utilisateur=?";
-	private static final String UPDATE_BOOL_GAGNANTE = "update enchere set enchere_gagnante=? where no_article =?";
+private static final String SELECT_ENCHERE_BY_ID_ARTICLE = "SELECT  e.no_enchere,e.date_enchere, e.montant_enchere,e.no_article,e.no_utilisateur FROM enchere AS e WHERE no_article=?";
+private static final String INSERT_ENCHERE = " INSERT INTO enchere (date_enchere, montant_enchere,no_utilisateur,no_article) VALUES (?,?,?,?)";
+private static final String SELECT_MAX_ENCHERE= "select no_enchere, date_enchere, MAX(montant_enchere),no_utilisateur,no_article, enchere_gagnante from enchere WHERE no_article=?";
+private static final String SELECT_MAX_UTILISATEUR= "select no_enchere, date_enchere, MAX(montant_enchere),no_utilisateur,no_article, enchere_gagnante from enchere WHERE no_utilisateur=?";
+private static final String UPDATE_BOOL_GAGNANTE = "update enchere set enchere_gagnante=? where no_article =?";
 
-	// méthode pour insérer une enchère en BDD
+	//	méthode pour insérer une enchère en BDD
 	@Override
 	public void insertEnchere(Enchere enchere) throws BusinessException {
-		if (enchere == null || enchere.getMontantEnchere() == null || enchere.getDateEnchere() == null) {
+		if(enchere == null || enchere.getMontantEnchere() == null || enchere.getDateEnchere() == null) {
 			BusinessException businessException = new BusinessException();
 			businessException.ajouterErreur(CodesResultatDAL.INSERT_ENCHERE_NULL);
 			throw businessException;
 		}
-
-		try (Connection cnx = ConnectionProvider.getConnection()) {
+		
+		try (Connection cnx = ConnectionProvider.getConnection()){
 			PreparedStatement pstmt = cnx.prepareStatement(INSERT_ENCHERE, PreparedStatement.RETURN_GENERATED_KEYS);
 			pstmt.setTimestamp(1, java.sql.Timestamp.valueOf(enchere.getDateEnchere()));
 			pstmt.setInt(2, enchere.getMontantEnchere());
 			pstmt.setInt(3, enchere.getUtilisateur().getNoUtilisateur());
 			pstmt.setInt(4, enchere.getArticle().getNoArticle());
 			int nb = pstmt.executeUpdate();
-			if (nb > 0) {
+			if(nb>0) {
 				ResultSet rs = pstmt.getGeneratedKeys();
-				if (rs != null && rs.next()) {
+				if(rs != null && rs.next()) {
 					enchere.setNoEnchere(rs.getInt(1));
 				}
 			}
@@ -52,26 +52,27 @@ public class EnchereDAOJdbcImpl implements EnchereDAO {
 	@Override
 	public List<Enchere> selectEnchereByIdArticle(ArticleVendu articleVendu) throws BusinessException {
 		List<Enchere> result = new ArrayList<>();
-		if (articleVendu == null) {
+		if(articleVendu == null) {
 			BusinessException businessException = new BusinessException();
 			businessException.ajouterErreur(CodesResultatDAL.SELECT_ENCHERE_PARAMETER_NULL);
 			throw businessException;
 		}
-		try (Connection cnx = ConnectionProvider.getConnection()) {
+		try (Connection cnx = ConnectionProvider.getConnection()){
 			PreparedStatement pstmt = cnx.prepareStatement(SELECT_ENCHERE_BY_ID_ARTICLE);
 			pstmt.setInt(1, articleVendu.getNoArticle());
-
+			
 			ResultSet rs = pstmt.executeQuery();
-
-			while (rs.next()) {
-
+			
+			
+			
+			while(rs.next()) {
+			
 				Integer idUtil = rs.getInt("no_utilisateur");
 				Integer idArticle = rs.getInt("no_article");
-
+				
 				ArticleVendu articleVendu1 = (ArticleVendu) new ArticleVenduDAOJdbcImpl().selectByIdArticle(idArticle);
 				Utilisateur utilisateur = (Utilisateur) new UtilisateurDAOJdbcImpl().selectByIdUtilisateur(idUtil);
-				result.add(new Enchere(rs.getInt("no_enchere"), rs.getTimestamp("date_enchere").toLocalDateTime(),
-						rs.getInt("montant_enchere"), articleVendu1, utilisateur));
+				result.add(new Enchere(rs.getInt("no_enchere"),rs.getTimestamp("date_enchere").toLocalDateTime(),rs.getInt("montant_enchere"),articleVendu1,utilisateur));
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -86,85 +87,82 @@ public class EnchereDAOJdbcImpl implements EnchereDAO {
 	@Override
 	public Enchere selectMaxEnchere(ArticleVendu articleVendu) throws BusinessException {
 		Enchere ench = null;
-		if (articleVendu == null) {
+		if(articleVendu == null) {
 			BusinessException businessException = new BusinessException();
 			businessException.ajouterErreur(CodesResultatDAL.SELECT_ENCHERE_PARAMETER_NULL);
 			throw businessException;
 		}
-		try (Connection cnx = ConnectionProvider.getConnection()) {
+		try (Connection cnx = ConnectionProvider.getConnection()){
 			PreparedStatement pstmt = cnx.prepareStatement(SELECT_MAX_ENCHERE);
 			pstmt.setInt(1, articleVendu.getNoArticle());
+			
 			ResultSet rs = pstmt.executeQuery();
-
-			while (rs.next()) {
-
+			
+			
+			
+			while(rs.next()) {
+			
 				Integer idUtil = rs.getInt("no_utilisateur");
 				Integer idArticle = rs.getInt("no_article");
-
+				
 				ArticleVendu articleVendu1 = (ArticleVendu) new ArticleVenduDAOJdbcImpl().selectByIdArticle(idArticle);
 				Utilisateur utilisateur = (Utilisateur) new UtilisateurDAOJdbcImpl().selectByIdUtilisateur(idUtil);
-				Integer gagnante = rs.getInt(6);
+				Integer gagnante= rs.getInt(6);
 				Boolean enchGa;
-				if (gagnante == 0) {
-					enchGa = false;
-
+				if (gagnante==0) {
+					enchGa=false;
+					
 				} else {
-					enchGa = true;
+					enchGa=true;
 				}
+				
 
-				if (idUtil != null) {
-					ench = new Enchere(rs.getInt("no_enchere"), rs.getTimestamp("date_enchere").toLocalDateTime(),
-							rs.getInt(3), articleVendu1, utilisateur, enchGa);
-				} else {
-					ench = null;
-				}
+				ench= new Enchere(rs.getInt("no_enchere"),rs.getTimestamp("date_enchere").toLocalDateTime(),rs.getInt(3),articleVendu1,utilisateur, enchGa);
 			}
-	}catch(
-
-	Exception e)
-	{
-		e.printStackTrace();
-		BusinessException businessException = new BusinessException();
-		businessException.ajouterErreur(CodesResultatDAL.ECHEC_SELECT_MAX_ENCHERE);
-		throw businessException;
-	}return ench;
+		} catch (Exception e) {
+			e.printStackTrace();
+			BusinessException businessException = new BusinessException();
+			businessException.ajouterErreur(CodesResultatDAL.ECHEC_SELECT_MAX_ENCHERE);
+			throw businessException;
+		}
+		return ench;
 	}
 
 	@Override
 	public List<Enchere> selectEnchereByIdUtilisateur(ArticleVendu articleVendu) throws BusinessException {
 		List<Enchere> ench = null;
-		if (articleVendu == null) {
+		if(articleVendu == null) {
 			BusinessException businessException = new BusinessException();
 			businessException.ajouterErreur(CodesResultatDAL.SELECT_ENCHERE_PARAMETER_NULL);
 			throw businessException;
 		}
-		try (Connection cnx = ConnectionProvider.getConnection()) {
+		try (Connection cnx = ConnectionProvider.getConnection()){
 			PreparedStatement pstmt = cnx.prepareStatement(SELECT_MAX_UTILISATEUR);
 			pstmt.setInt(1, articleVendu.getNoArticle());
-
+			
 			ResultSet rs = pstmt.executeQuery();
-
-			while (rs.next()) {
-
+			
+			
+			
+			while(rs.next()) {
+			
 				Integer idUtil = rs.getInt("no_utilisateur");
 				Integer idArticle = rs.getInt("no_article");
-
+				
 				ArticleVendu articleVendu1 = (ArticleVendu) new ArticleVenduDAOJdbcImpl().selectByIdArticle(idArticle);
 				Utilisateur utilisateur = (Utilisateur) new UtilisateurDAOJdbcImpl().selectByIdUtilisateur(idUtil);
-				Integer gagnante = rs.getInt(6);
+				Integer gagnante= rs.getInt(6);
 				Boolean enchGa;
-				if (gagnante == 0) {
-					enchGa = false;
-
+				if (gagnante==0) {
+					enchGa=false;
+					
 				} else {
-					enchGa = true;
+					enchGa=true;
 				}
-				if (ench == null) {
-					ench = new ArrayList<Enchere>();
-					;
-				}
-				ench.add(new Enchere(rs.getInt("no_enchere"), rs.getTimestamp("date_enchere").toLocalDateTime(),
-						rs.getInt(3), articleVendu1, utilisateur, enchGa));
+				if (ench==null) {
+					ench= new ArrayList<Enchere>();
+;				}
+				ench.add(new Enchere(rs.getInt("no_enchere"),rs.getTimestamp("date_enchere").toLocalDateTime(),rs.getInt(3),articleVendu1,utilisateur,enchGa));
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -179,33 +177,33 @@ public class EnchereDAOJdbcImpl implements EnchereDAO {
 	public List<Enchere> selectAllEnchereGagneeByIdU(Utilisateur util) throws BusinessException {
 		List<Enchere> ench = null;
 
-		try (Connection cnx = ConnectionProvider.getConnection()) {
+		try (Connection cnx = ConnectionProvider.getConnection()){
 			PreparedStatement pstmt = cnx.prepareStatement(SELECT_MAX_UTILISATEUR);
 			pstmt.setInt(1, util.getNoUtilisateur());
-
+			
 			ResultSet rs = pstmt.executeQuery();
-
-			while (rs.next()) {
-
+			
+			
+			
+			while(rs.next()) {
+			
 				Integer idUtil = rs.getInt("no_utilisateur");
 				Integer idArticle = rs.getInt("no_article");
-
+				
 				ArticleVendu articleVendu1 = (ArticleVendu) new ArticleVenduDAOJdbcImpl().selectByIdArticle(idArticle);
 				Utilisateur utilisateur = (Utilisateur) new UtilisateurDAOJdbcImpl().selectByIdUtilisateur(idUtil);
-				Integer gagnante = rs.getInt(6);
+				Integer gagnante= rs.getInt(6);
 				Boolean enchGa;
-				if (gagnante == 0) {
-					enchGa = false;
-
+				if (gagnante==0) {
+					enchGa=false;
+					
 				} else {
-					enchGa = true;
+					enchGa=true;
 				}
-				if (ench == null) {
-					ench = new ArrayList<Enchere>();
-					;
-				}
-				ench.add(new Enchere(rs.getInt("no_enchere"), rs.getTimestamp("date_enchere").toLocalDateTime(),
-						rs.getInt(3), articleVendu1, utilisateur, enchGa));
+				if (ench==null) {
+					ench= new ArrayList<Enchere>();
+;				}
+				ench.add(new Enchere(rs.getInt("no_enchere"),rs.getTimestamp("date_enchere").toLocalDateTime(),rs.getInt(3),articleVendu1,utilisateur,enchGa));
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -227,15 +225,17 @@ public class EnchereDAOJdbcImpl implements EnchereDAO {
 			PreparedStatement pstmt = cnx.prepareStatement(UPDATE_BOOL_GAGNANTE);
 
 			Boolean enchGa = enchere.getEnchereGagnante();
-
+			
 			if (enchGa) {
-
+				
 				pstmt.setInt(1, 1);
 			} else {
 				pstmt.setInt(1, 0);
 			}
+			
 
 			pstmt.setInt(2, enchere.getArticle().getNoArticle());
+
 
 			pstmt.executeUpdate();
 
@@ -248,4 +248,7 @@ public class EnchereDAOJdbcImpl implements EnchereDAO {
 			throw businessException;
 		}
 	}
-}
+	}
+
+
+
